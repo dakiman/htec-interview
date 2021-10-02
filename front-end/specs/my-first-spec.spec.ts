bd2f9334358369a3be42039c9e78695b29191dd8
dakiman <dvancov@hotmail.com>
2021-10-02 23:35:43 +0200

    add sidebar component for nav

--- front-end/specs/my-first-spec.spec.ts
+++ front-end/specs/my-first-spec.spec.ts
@@ -1,9 +1,11 @@
 import BrowserUtils from "../utils/WebDriverUtils";
 import LoginActions from "../actions/LoginActions";
+import SidebarComponent from "../pages/SidebarComponent";
 
 describe('My front-end test suite', () => {
 
     let loginActions = new LoginActions();
+    let sidebarComponent = new SidebarComponent();
 
     beforeAll(async () => {
         await loginActions.defaultLogin();
@@ -12,6 +14,12 @@ describe('My front-end test suite', () => {
     it('Is logged in', async () => {
         let isUrlDashboard = await BrowserUtils.doesUrlContain('/dashboard')
         expect(isUrlDashboard).toBe(true);
+    })
+
+    it('Navigates to Test Cases tab', async () => {
+        await sidebarComponent.clickTestCasesNavButton();
+        let isUrlTestCases = await BrowserUtils.doesUrlContain('/testcases')
+        expect(isUrlTestCases).toBe(true);
     })
 
 })
