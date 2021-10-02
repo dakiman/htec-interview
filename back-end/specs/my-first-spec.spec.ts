8e8103229f73fad40670a7ae86b776bd859dbddb
dakiman <dvancov@hotmail.com>
2021-10-02 02:42:09 +0200

    backend skeleton added

--- back-end/specs/my-first-spec.spec.ts
+++ back-end/specs/my-first-spec.spec.ts
@@ -1,3 +1,24 @@
-describe('My backend test suite', () => {
+import axios from "axios";
 
+
+
+describe('My backend test suite', () => {
+    it('Does smth', async () => {
+        let res = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+
+        console.log(res.data);
+
+
+
+        expect(res.data).toBeNull();
+    })
+
+    it('Does smth', async () => {
+        let res = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+
+        console.log(res.data);
+
+        expect(res.data).toBe(1);
+    })
 })
+
