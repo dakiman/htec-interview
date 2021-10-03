3539d8863adaa146707a5c98f3eb3e56754c6cc5
dakiman <dvancov@hotmail.com>
2021-10-03 03:30:11 +0200

    delete cases, inline code

--- front-end/specs/add-test-cases.spec.ts
+++ front-end/specs/add-test-cases.spec.ts
@@ -5,6 +5,7 @@ import WebDriverUtils from "../utils/WebDriverUtils";
 import DataUtils from "../utils/DataUtils";
 import TestCase from "../types/TestCase";
 import TestCaseActions from "../actions/TestCaseActions";
+import {$$, browser, by, element} from "protractor";
 
 describe('Add test cases to application', () => {
 
@@ -20,8 +21,26 @@ describe('Add test cases to application', () => {
         testCases = JSON.parse(await DataUtils.readFileAsJson('front-end/resources/testCases.json'));
     })
 
-    it('Add test cases from a file', async () => {
-        await testCaseActions.addTestCases(testCases);
+    it('Deletes existing test cases', async () => {
+        await browser.sleep(2000)
+        let testCases = $$('.portrait-grid > span');
+        let casesCount = await testCases.count();
+
+        for (let i = 0; i < casesCount; i++) {
+            testCases = $$('.portrait-grid > span');
+            testCases.first().element(by.xpath('//*[contains(@href, "/edit-testcase/")]')).click();
+            let deleteBtn = element(by.css('#root > div > div.grid-menu-container > div.main-grid > div > div > div:nth-child(1) > div > div > div > span > button'))
+            deleteBtn.click();
+
+            let removeBtn = element(by.className('confirmation-dialog--buttons--confirm'))
+            removeBtn.click();
+
+            await WebDriverUtils.waitUntilUrlContains('/testcases')
+        }
     })
+
+    // it('Add test cases from a file', async () => {
+    //     await testCaseActions.addTestCases(testCases);
+    // })
 
 })
